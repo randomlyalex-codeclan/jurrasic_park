c0584bedaab29a745d77fb6339520e3020af725d
Alex Dodd <alexdodd@gmail.com>
2021-01-07 00:37:18 +0000

    test 4 written and passed

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -1,7 +1,12 @@
 const Park = function (name, entryPrice) {
     this.name = name;
     this.entryPrice = entryPrice;
+    this.dinosaurCollection = [];
 
   }
   
+Park.prototype.addDinosaur = function (dinosaur) {
+    this.dinosaurCollection.push(dinosaur)
+}
+
   module.exports = Park;
